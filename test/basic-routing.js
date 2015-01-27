cf59599492b8d70a2bf977ae874809fcdd0fbc62
Moshe Kolodny <mkolodny@integralads.com>
2015-01-27 15:58:16 -0500

    only bind to hashchange when using window.location

--- test/basic-routing.js
+++ test/basic-routing.js
@@ -6,30 +6,58 @@ var wrt = require('..');
 var Router = wrt.Router;
 var Route = wrt.Route;
 
-describe('A basic router', function() {
+describe('A basic hash router', function() {
   describe('when embedded directly on the DOM', function() {
-    var routerElement;
-    var router;
 
-    beforeEach(function() {
-      routerElement = (
-        <Router location={{href:'http://www.google.com/'}}>
-          <Route path="/" handler={function() { return <h1>Root</h1> }} />
-          <Route path="/sub" handler={function() { return <h2>Sub</h2> }} />
-        </Router>
-      );
-      router = React.render(routerElement, document.body);
+    describe('when the location is "http://www.google.com/"', function() {
+      var routerElement;
+      var router;
+
+      beforeEach(function() {
+        routerElement = (
+          <Router root="#/" location={{href:'http://www.google.com/'}}>
+            <Route path="/" handler={function() { return <h1>Root</h1>; }} />
+            <Route path="/sub" handler={function() { return <h2>Sub</h2>; }} />
+          </Router>
+        );
+        router = React.render(routerElement, document.body);
+      });
+
+      afterEach(function(done) {
+        React.unmountComponentAtNode(document.body);
+        setTimeout(done);
+      });
+
+      it('should default to the root route', function() {
+        assert.equal(router.getDOMNode().textContent, 'Root');
+      });
     });
 
-    afterEach(function(done) {
-      React.unmountComponentAtNode(document.body);
-      setTimeout(done);
-    });
+    // describe('when the location is "http://www.google.com/#/sub"', function() {
+    //   var routerElement;
+    //   var router;
 
-    it('should default to the root route', function() {
-      console.log('!', router.getDOMNode().outerHTML, '!');
-    });
+    //   beforeEach(function() {
+    //     routerElement = (
+    //       <Router root="#/" location={{href:'http://www.google.com/#/sub'}}>
+    //         <Route path="/" handler={function() { return <h1>Root</h1>; }} />
+    //         <Route path="/sub" handler={function() { return <h2>Sub</h2>; }} />
+    //       </Router>
+    //     );
+    //     router = React.render(routerElement, document.body);
+    //   });
+
+    //   afterEach(function(done) {
+    //     React.unmountComponentAtNode(document.body);
+    //     setTimeout(done);
+    //   });
+
+    //   it('should default to the root route', function() {
+    //     assert.equal(router.getDOMNode().textContent, 'Sub');
+    //   });
+    // });
 
   });
+
 });
 
