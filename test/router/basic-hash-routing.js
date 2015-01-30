437eb87f50e0d89adfb6cff6955f4f4874ecb6df
Moshe Kolodny <mkolodny@integralads.com>
2015-01-29 22:59:28 -0500

    added hashchange and popstate

--- test/router/basic-hash-routing.js
+++ test/router/basic-hash-routing.js
@@ -6,6 +6,8 @@ var wrt = require('../..');
 var Router = wrt.Router;
 var Route = wrt.Route;
 
+var theGlobal = Function('return this')() || (42, eval)('this');
+
 describe('A basic hash router', function() {
   describe('when embedded directly on the DOM', function() {
 
@@ -70,6 +72,43 @@ describe('A basic hash router', function() {
       });
 
     });
+
+    describe('when the location is the global `location`', function() {
+      var oldAddEventListener = theGlobal.addEventListener;
+      var calledInfo;
+      var routerElement;
+      var router;
+      beforeEach(function() {
+        if (!theGlobal.location) {
+          theGlobal.location = { href: 'http://www.google.com/#/' };
+        }
+        calledInfo = { called: 0 };
+        global.onhashchange = null;
+        global.addEventListener = function() {
+          calledInfo.args = arguments;
+          calledInfo.called++;
+        };
+
+        routerElement = (
+          <Router root="#/"><Route handler={function() { return <br /> }} /></Router>
+        );
+        router = React.render(routerElement, document.body);
+
+      });
+
+      afterEach(function(done) {
+        theGlobal.addEventListener = oldAddEventListener;
+        React.unmountComponentAtNode(document.body);
+        setTimeout(done);
+      });
+
+      it('set up a hashchange event listener', function() {
+        assert(calledInfo.called, 1);
+        assert(calledInfo.args[0] === 'hashchange');
+      });
+
+    });
+
 
   });
 
--- test/router/basic-path-routing.js
+++ test/router/basic-path-routing.js
@@ -6,6 +6,8 @@ var wrt = require('../..');
 var Router = wrt.Router;
 var Route = wrt.Route;
 
+var theGlobal = Function('return this')() || (42, eval)('this');
+
 describe('A basic path router', function() {
   describe('when embedded directly on the DOM', function() {
 
@@ -67,6 +69,42 @@ describe('A basic path router', function() {
         var newLocation = { href: 'http://www.google.com/' };
         router.setState({location: newLocation});
         assert.equal(router.getDOMNode().textContent, 'Root');
+      });
+
+    });
+
+    describe('when the location is the global `location`', function() {
+      var oldAddEventListener = theGlobal.addEventListener;
+      var calledInfo;
+      var routerElement;
+      var router;
+      beforeEach(function() {
+        if (!theGlobal.location) {
+          theGlobal.location = { href: 'http://www.google.com/' };
+        }
+        calledInfo = { called: 0 };
+        global.onpopstate = null;
+        global.addEventListener = function() {
+          calledInfo.args = arguments;
+          calledInfo.called++;
+        };
+
+        routerElement = (
+          <Router><Route handler={function() { return <br /> }} /></Router>
+        );
+        router = React.render(routerElement, document.body);
+
+      });
+
+      afterEach(function(done) {
+        theGlobal.addEventListener = oldAddEventListener;
+        React.unmountComponentAtNode(document.body);
+        setTimeout(done);
+      });
+
+      it('set up a hashchange event listener', function() {
+        assert(calledInfo.called, 1);
+        assert(calledInfo.args[0] === 'popstate');
       });
 
     });
